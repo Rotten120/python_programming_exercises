b965fbbae393ea07042c9a612236dc4c354caee0
Von <vonzedric012@gmail.com>
2025-03-12 17:10:51 +0800

    copied from folder batch_1

---
+++ get_input.py
@@ -0,0 +1,30 @@
+def numbers(arr_size = 2):
+    arr_input = []
+    print("Input", arr_size, "numbers")
+    print("Float numbers are rounded")
+    for idx in range(arr_size):
+        try:
+            inp_num = [int(input("Enter number: "))]
+            arr_input += inp_num
+        except:
+            continue
+        
+    if len(arr_input) == 0:
+        return [-1]
+    return arr_input
+
+def nums_until_invalid():
+    arr_input = []
+    print("Input numbers")
+    print("Float numbers are rounded")
+    print("Invalid inputs stops inputting process")
+    while True:
+        try:
+            inp_num = [int(input("Enter number: "))]
+            arr_input += inp_num
+        except:
+            break
+        
+    if len(arr_input) == 0:
+        return [-1]
+    return arr_input
