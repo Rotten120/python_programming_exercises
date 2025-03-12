d2c06f76dac121614a0bac30e89895da32c441af
Von <vonzedric012@gmail.com>
2025-03-12 15:57:53 +0800

    Added instruction msgs

--- batch_1/get_input.py
+++ batch_1/get_input.py
@@ -1,5 +1,7 @@
 def numbers(arr_size = 2):
     arr_input = []
+    print("Input", arr_size, "numbers")
+    print("Float numbers are rounded")
     for idx in range(arr_size):
         try:
             inp_num = [int(input("Enter number: "))]
@@ -10,6 +12,8 @@ def numbers(arr_size = 2):
 
 def floats_until_invalid():
     arr_input = []
+    print("Input numbers")
+    print("Invalid inputs stops inputting process")
     while True:
         try:
             inp_num = [float(input("Enter number: "))]
