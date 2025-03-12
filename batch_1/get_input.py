625908006ba1617301d30f6ab0be2dc082719c0e
Von <vonzedric012@gmail.com>
2025-03-12 13:59:00 +0800

    Fixed bug: only returns the last value inputted

--- batch_1/get_input.py
+++ batch_1/get_input.py
@@ -1,9 +1,9 @@
-def get_input(arr_size = 2):
+def numbers(arr_size = 2):
     arr_input = []
     for idx in range(arr_size):
         try:
-            inp_num = [float(input("Enter number: ")]
-            arr_input = inp_num
+            inp_num = [int(input("Enter number: "))]
+            arr_input += inp_num
         except:
             continue
     return arr_input
