821ae724b517d56e58983132156b38637eda6964
Von <vonzedric012@gmail.com>
2025-03-12 15:32:02 +0800

    is_num_uniqe_or_duply.py uploaded

---
+++ batch_3/is_num_unique_or_dupli.py
@@ -0,0 +1,18 @@
+import get_input
+
+def is_num_unique_or_dupli(arr):
+    nums_in_arr = []
+    duped_nums = []
+
+    for num in arr:
+        if num in nums_in_arr: duped_nums += [num]
+        else: nums_in_arr += [num]
+
+    for num in arr:
+        num_identifier = "Duplicate" if num in duped_nums else "Unique"
+        print(num, num_identifier)
+
+if __name__ == "__main__":
+    arr_inp = get_input.floats_until_invalid()
+    print("\nResults")
+    is_num_unique_or_dupli(arr_inp)
