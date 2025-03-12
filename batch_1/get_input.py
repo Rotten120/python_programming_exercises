4c3e1278e9c7e8b86231660e674c2ab96de80e46
Von <vonzedric012@gmail.com>
2025-03-12 14:58:09 +0800

    added function for a continuous input until input is invalid

--- batch_1/get_input.py
+++ batch_1/get_input.py
@@ -7,3 +7,13 @@ def numbers(arr_size = 2):
         except:
             continue
     return arr_input
+
+def floats_until_invalid():
+    arr_input = []
+    while True:
+        try:
+            inp_num = [float(input("Enter number: "))]
+            arr_input += inp_num
+        except:
+            break
+    return arr_input
